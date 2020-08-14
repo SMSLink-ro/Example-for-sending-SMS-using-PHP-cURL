f91b508e40cb51358781f17de9c70675b3d37b00
SMSLink.ro <contact@smslink.ro>
2020-08-14 07:01:26 +0000

    Update

--- main.php
+++ main.php
@@ -15,13 +15,13 @@ $smsTo = "07xyzzzzzz";
 $smsMessage = "My Test Message";
 
 curl_setopt_array($curl, array(
-	CURLOPT_URL => "https://secure.smslink.ro/sms/gateway/communicate/index.php".
-    "?connection_id=".$connectionId.
-    "&password=".$password.
-    "&to=".$smsTo.
-    "&message=".urlencode($smsMessage),
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_URL => "https://secure.smslink.ro/sms/gateway/communicate/index.php".
+"?connection_id=".$connectionId.
+"&password=".$password.
+"&to=".$smsTo.
+"&message=".urlencode($smsMessage),
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HEADER => false,
   CURLOPT_SSL_VERIFYPEER => false,
   CURLOPT_SSL_VERIFYHOST => false
